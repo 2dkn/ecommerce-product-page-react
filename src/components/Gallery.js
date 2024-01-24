07127242d2521a0b5b4a7588a32e69f464ab574a
2dkn <2dkneely@gmail.com>
2024-01-24 12:04:34 -0500

    Added state to images and thumbnail. Changer header to outline. Next is Count and Cart button state

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,19 +1,54 @@
-import React from "react";
-
+import React, { useState } from "react";
 import { image1, image2, image3, image4 } from "../images";
 import { thumbnail1, thumbnail2, thumbnail3, thumbnail4 } from "../images";
 
 function Gallery() {
+  const [selectedImage, setSelectedImage] = useState(image1);
+  const [selectedThumbnail, setSelectedThumbnail] = useState(thumbnail1);
+
+  const handleThumbnailClick = (newImage, newThumbnail) => {
+    setSelectedImage(newImage);
+    setSelectedThumbnail(newThumbnail);
+  };
+
   return (
     <div className="image-container">
       <div className="big-images">
-        <img className="big-sneaker" src={image1} alt="big sneaker" />
+        <img className="big-sneaker" src={selectedImage} alt="big sneaker" />
       </div>
       <div className="small-images">
-        <img className="small-sneaker" src={thumbnail1} alt="small sneaker" />
-        <img className="small-sneaker" src={thumbnail2} alt="small sneaker" />
-        <img className="small-sneaker" src={thumbnail3} alt="small sneaker" />
-        <img className="small-sneaker" src={thumbnail4} alt="small sneaker" />
+        <img
+          className={`small-sneaker ${
+            selectedThumbnail === thumbnail1 ? "selected" : ""
+          }`}
+          src={thumbnail1}
+          alt="small sneaker"
+          onClick={() => handleThumbnailClick(image1, thumbnail1)}
+        />
+        <img
+          className={`small-sneaker ${
+            selectedThumbnail === thumbnail2 ? "selected" : ""
+          }`}
+          src={thumbnail2}
+          alt="small sneaker"
+          onClick={() => handleThumbnailClick(image2, thumbnail2)}
+        />
+        <img
+          className={`small-sneaker ${
+            selectedThumbnail === thumbnail3 ? "selected" : ""
+          }`}
+          src={thumbnail3}
+          alt="small sneaker"
+          onClick={() => handleThumbnailClick(image3, thumbnail3)}
+        />
+        <img
+          className={`small-sneaker ${
+            selectedThumbnail === thumbnail4 ? "selected" : ""
+          }`}
+          src={thumbnail4}
+          alt="small sneaker"
+          onClick={() => handleThumbnailClick(image4, thumbnail4)}
+        />
       </div>
     </div>
   );
