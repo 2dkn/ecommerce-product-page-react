59fe1cf5d306dcc23c4c39de3a9234ea72ba52dc
2dkn <2dkneely@gmail.com>
2024-01-28 13:46:16 -0500

    Added Cart Overlay for checkout. Need to add state next from count into checkout counter.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "../images/logo.svg";
 import avatar from "../images/image-avatar.png";
 import { ReactComponent as Cart } from "../images/icon-cart.svg";
+import { ReactComponent as Trash } from "../images/icon-delete.svg";
+import sneaker from "../images/image-product-1-thumbnail.jpg";
 
 function Header() {
+  const [isCheckoutOpen, setCheckoutOpen] = useState(false);
+
+  const toggleCheckout = () => {
+    setCheckoutOpen(!isCheckoutOpen);
+  };
+
   return (
     <header className="header-container">
       <div className="header-left">
@@ -25,9 +33,28 @@ function Header() {
         </a>
       </div>
       <div className="header-right">
-        <button className="cart">
+        <button className="cart" onClick={toggleCheckout}>
           <Cart fill="#69707D" />
         </button>
+        {isCheckoutOpen && (
+          <div className="checkout">
+            <div>
+              <h1>Cart</h1>
+            </div>
+            <div className="price-count">
+              <img className="sneaker-cart" src={sneaker}></img>
+              <p>Fall Limited Edition Sneakers</p>
+              <p>$125.00 x 1</p>
+              <p>Total Price</p>
+              <button className="trash">
+                <Trash />
+              </button>
+            </div>
+            <div>
+              <button className="checkout-btn">Checkout</button>
+            </div>
+          </div>
+        )}
         <img className="avatar" src={avatar} alt="Man glasses" />
       </div>
     </header>
